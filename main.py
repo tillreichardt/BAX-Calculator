5869e63878ee2a1e812ef2ed73d71cdd1318c71d
Till Reichardt <till@reichardt.io>
2025-12-10 16:04:16 +0100

    new name for bax calculation in player class

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from src.data.spieler_daten import spielerListe
 
 spieler = spielerListe[0]
 print(f"Name: {spieler.vorname} {spieler.nachname}")
-spieler.baxBerechnung()
+# spieler.baxBerechnungEinzel()
+spieler.baxBerechnungDoppel()
 print("-" * 20 + "\n\n")
