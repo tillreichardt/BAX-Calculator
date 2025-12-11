b576e7e6c78ab30d6dfa5fbb94abede159f74ad9
Till Reichardt <till@reichardt.io>
2025-12-11 14:14:45 +0100

    more player data

--- src/data/spieler_daten.py
+++ src/data/spieler_daten.py
@@ -60,5 +60,54 @@ spielerListe = [
             #     ergebnis=[1,2]
             # )
         ]
+    ),
+    Spieler(
+        "Jonas",
+        "Klose",
+        "BG 62 Dormagen",
+        gegner_daten_einzel=[
+            EinzelMatch(
+                gegner_bax= get_bax("Atif", "Javed", "Osterather TV", "einzel"),
+                ergebnis=[2,1]
+            ),
+            EinzelMatch(
+                gegner_bax= get_bax("Stephan", "Vallata", "SC Bad Bears BSC", "einzel"),
+                ergebnis=[2,0]
+            ),
+            EinzelMatch(
+                gegner_bax= get_bax("Manuel", "Günster", "SV DJK Holzbüttgen", "einzel"),
+                ergebnis=[2,0]
+            ),
+            EinzelMatch(
+                gegner_bax= get_bax("Alexander", "Reinhardt", "TSV Norf", "einzel"),
+                ergebnis=[2,0]
+            )
+        ],
+        gegner_daten_doppel=[
+            DoppelMatch(
+                partner_bax= get_bax("Lukas", "Bartsch", "BG 62 Dormagen", "doppel"),
+                gegner_bax_1= get_bax("Atif", "Javed", "Osterather TV", "doppel"),
+                gegner_bax_2= get_bax("Muhammad", "Ahmad", "Osterather TV", "doppel"),
+                ergebnis=[0,2]
+            ),
+            DoppelMatch(
+                partner_bax= get_bax("Lukas", "Bartsch", "BG 62 Dormagen", "doppel"),
+                gegner_bax_1= get_bax("Heinz-Josef", "Rösch", "SC Bad Bears BSC", "doppel"),
+                gegner_bax_2= get_bax("Stephan", "Vallata", "SC Bad Bears BSC", "doppel"),
+                ergebnis=[2,0]
+            ),
+            DoppelMatch(
+                partner_bax= get_bax("Lukas", "Bartsch", "BG 62 Dormagen", "doppel"),
+                gegner_bax_1= get_bax("Renaldi", "Bernard", "SV DJK Holzbüttgen", "doppel"),
+                gegner_bax_2= get_bax("Manuel", "Günster", "DV DJK Holzbüttgen", "doppel"),
+                ergebnis=[2,1]
+            ),
+            DoppelMatch(
+                partner_bax= get_bax("Lukas", "Bartsch", "BG 62 Dormagen", "doppel"),
+                gegner_bax_1= get_bax("Alexander", "Reinhardt", "TSV Norf", "doppel"),
+                gegner_bax_2= get_bax("Florian", "Klein", "TSV Norf", "doppel"),
+                ergebnis=[2,0]
+            )
+        ]
     )
 ]
