301ea8ba59d88c8be6ab9704797325036e664068
Till Reichardt <till@reichardt.io>
2026-03-19 10:18:39 +0100

    remove doubles calculation

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from src.data.spieler_daten import spielerListe
 
 for spieler in spielerListe:
-    print(f"Name: {spieler.vorname} {spieler.nachname}")
+    print(f"\nName: {spieler.vorname} {spieler.nachname}")
     spieler.baxBerechnungEinzel()
     print("-" * 20 + "\n\n")
-    # spieler.baxBerechnungDoppel()
-    print("-" * 20 + "\n\n")
-    
